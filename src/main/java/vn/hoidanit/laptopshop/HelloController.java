6807b342be0bcee5a6bbd3e18817ef3b5da220dd
Nhat <trinhthanhnhat123@gmail.com>
2025-11-01 16:21:06 +0700

    #33-phan trang HelloController

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
     @GetMapping("/")
     public String index() {
-        return "Hello World zz!";
+        return "Hello !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only User !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin !";
     }
 }
