13a7f008bea9877c04c0a1d1b71b1dd14d0e6a60
Nhat <trinhthanhnhat123@gmail.com>
2025-11-01 18:10:08 +0700

    exclude temp function login

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,7 +3,12 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+//include (bao gồm) >< exclude (loại bỏ)
+
+//loại bỏ tạm thời chức năng đăng nhập
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
