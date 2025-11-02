6ded79e4a99451b8a7edd9a1cfed225f9dc7b7f7
Nhat <trinhthanhnhat123@gmail.com>
2025-11-03 01:24:54 +0700

    #46 static is view, cac file html

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,35 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-/*@controller là 1 annotation: trang trí một class của java, giúp "tăng thêm sức mạnh" tại sao lại tăng thêm sức mạnh
-tại vì khi chạy nó sẽ đọc các annotation nó sẽ truyền thêm cái "metadata"
-*/
-
-// @Controller
-// public class UserController {
-//     // function ứng với từng view, để làm được thì sẽ làm 1 cái class/method
-//     // để truy cập cái method/class này thì 1 cái annotation tiếp theo ra đời mà
-//     // spring đã cấu hình sẵn chỉ việc lấy ra dùng
-//     // annotation đó là @RequestMapping nó giống như yêu cầu lên server(java), chúng
-//     // gửi từ đâu từ trình duyệt web
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from Controller";
-//     }
-//     //chạy lỗi là do chạy theo mô hình mvc
-// }
-
-@RestController
+@Controller
 public class UserController {
-    // DI: dependency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        return "eric.html";
     }
-}
+} 
