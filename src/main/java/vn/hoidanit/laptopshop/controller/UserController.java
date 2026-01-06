b6fd95c76d5492fbbf831f08a908cdf4fde7d294
Nhat <trinhthanhnhat123@gmail.com>
2026-01-06 23:41:45 +0700

    feat: add controller and model

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,23 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+
+    @GetMapping("")
+    public String getHomePage() {
+        return userService.handleHello();
+    }
+}
