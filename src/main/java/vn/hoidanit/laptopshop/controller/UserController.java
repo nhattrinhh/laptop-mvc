248453f89c6a54625476751f5249da35062ac9dd
Nhat <trinhthanhnhat123@gmail.com>
2026-01-07 00:32:42 +0700

    feat: add jstl

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,10 +1,8 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import vn.hoidanit.laptopshop.service.UserService;
 
 
@@ -17,8 +15,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("nhat", "Hello Nhat dep trai");
         return "hello";
     }
 }
