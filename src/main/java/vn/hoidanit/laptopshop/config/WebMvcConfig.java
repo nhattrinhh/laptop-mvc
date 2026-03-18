2ebfbc7439244e31b955f31b42f29faa0b61fe37
Nhat <trinhthanhnhat123@gmail.com>
2026-03-19 00:55:44 +0700

    page client

--- src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
+++ src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
@@ -35,6 +35,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
         registry.addResourceHandler("/images/**").addResourceLocations("/resources/images/");
+        registry.addResourceHandler("/client/**").addResourceLocations("/resources/client/");
     }
 
 }
